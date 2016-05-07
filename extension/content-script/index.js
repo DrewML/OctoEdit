5c39649b987d4e3589b0a2de7bc9790695036683
Andrew Levine <theandrewtizer@gmail.com>
2016-05-07 16:55:07 -0500

    Added CodeMirror. TODO: Cleanup when leaving tab

--- extension/content-script/index.js
+++ extension/content-script/index.js
@@ -1,11 +1,16 @@
+import './extension.css';
+import 'codemirror/mode/gfm/gfm';
+import codeMirror from 'codemirror';
 import tabHandler from './tab-handler';
 import onPageNav from './on-page-nav';
-import codeMirror from 'codemirror';
 
 const tabs = tabHandler();
 
 function onTabEnter(wrapper) {
-    const editor = codeMirror(wrapper);
+    const editor = codeMirror(wrapper, {
+        mode: 'gfm',
+        lineNumbers: true
+    });
 }
 
 function onTabLeave(form) {
