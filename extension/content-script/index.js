8ec93e01bd5d44d4f8a6b04c7cb2521aebd4e6fe
James Talmage <james@talmage.io>
2016-05-17 04:05:21 -0400

    add EVERYTHING!!!

--- extension/content-script/index.js
+++ extension/content-script/index.js
@@ -1,5 +1,41 @@
 import './extension.css';
 import codeMirror from 'codemirror';
+
+// The Sublime Keymap
+import 'codemirror/keymap/sublime';
+
+// Dialogs for search
+import 'codemirror/addon/dialog/dialog';
+
+// Better scrollbar stuff
+import 'codemirror/addon/scroll/annotatescrollbar';
+import 'codemirror/addon/scroll/simplescrollbars';
+
+// Search / Search & Replace
+import 'codemirror/addon/search/searchcursor';
+import 'codemirror/addon/search/matchesonscrollbar';
+import 'codemirror/addon/search/match-highlighter';
+import 'codemirror/addon/search/search';
+
+// Editor niceties
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/matchtags';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/trailingspace';
+import 'codemirror/addon/edit/continuelist';
+
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/comment/continuecomment';
+
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/fold/indent-fold';
+import 'codemirror/addon/fold/comment-fold';
+import 'codemirror/addon/fold/xml-fold';
+import 'codemirror/addon/fold/markdown-fold';
+
 import tabHandler from './tab-handler';
 import onPageNav from './on-page-nav';
 import onHotkey from './hotkey';
@@ -31,7 +67,18 @@ function onTabEnter(wrapper, form) {
         lineNumbers: true,
         autofocus: true,
         lineWrapping: true,
-        value: plainTextArea.value
+        value: plainTextArea.value,
+        gutters: ['CodeMirror-foldgutter'],
+        keyMap: 'sublime',
+        scrollBarStyle: 'overlay',
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        matchTags: true,
+        autoCloseTags: true,
+        showTrailingSpace: true,
+        continueComments: true,
+        highlightSelectionMatches: true,
+        foldGutter: true,
     });
 
     editor.on('change', () => {
